1043008c418c6a47f42f986f283367c0cc2e4972
nomanvaghela <nomanvaghela7@gmail.com>
2024-04-04 23:33:01 +0530

    sd

--- src/components/AlertPopUp.jsx
+++ src/components/AlertPopUp.jsx
@@ -6,19 +6,13 @@ const AlertPopUp = ({ item }) => {
   return (
     <div
       className="relative z-10"
+      onClick={() => setAlertPopUp(false)}
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true">
-      <div
-        onClick={() => setAlertPopUp(false)}
-        className=" inset-0 fixed bg-black bg-opacity-75 transition-opacity"
-      />
+      <div className=" inset-0 fixed bg-black bg-opacity-30 transition-opacity" />
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        {/* <div
-          onClick={() => setAlertPopUp(false)}
-          className="relative inset-0 bg-black bg-opacity-75 transition-opacity"
-        /> */}
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
@@ -42,7 +36,7 @@ const AlertPopUp = ({ item }) => {
                   <h3
                     className="text-base mt-2 font-semibold leading-6 text-gray-900"
                     id="modal-title">
-                    Are you sure you want to delete this task?
+                    Are you sure you want to delete this {item.task}?
                   </h3>
                   <div className="mt-2"></div>
                 </div>

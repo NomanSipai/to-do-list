e73ab099714543c16332cb92c80aa36a326e8374
nomanvaghela <nomanvaghela7@gmail.com>
2024-06-08 14:28:37 +0530

    sdsds

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import UpdateBox from "./components/UpdateBox";
 
 function App() {
   return (
-    <div className=" min-h-screen bg-[#f0f4f1] px-2 pt-2">
+    <div className=" min-h-screen container mx-auto bg-[#f0f4f1] px-4 pt-2">
       <div className="bg-[#4a694f] p-5 text-white font-bold text-xl ">
         To Do List App
       </div>
